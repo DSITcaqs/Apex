f3879e4f524d778834a9581f6c924b8234b1c0b8
Carlos Quintana <carlos@gntcapital.com>
2019-10-15 22:31:55 -0500

    Pushed new Landing banner into the branch.

--- src/components/home/backgroundPrincipal.js
+++ src/components/home/backgroundPrincipal.js
@@ -18,20 +18,14 @@ class BackgroundPrincipalIndex extends Component {
       <div className='full-width-image'>
         <div className='container-row-center'>
           <div className='container-info'>
-
-            <p className='title-home-container-principal'>BIENVENIDO A </p>
-            <p className='text-description-container-principal'>APEX</p>
-            <p className='subtext-description-container-principal'>Creación de riqueza rediseñada</p>
+            <p className='title-home-container-principal'>BIENVENIDO A APEX</p>
+            {/*<p className='text-description-container-principal'>APEX</p>*/}
+            <p className='subtext-description-container-principal'>Rediseñando la manera de invertir tu patrimonio.</p>
             <Button className='buttons-home' onClick={() => { this.redirectProducts() }}>Ver mas <Image src={Arrw} style={{ width: 18, marginLeft: 5, marginTop: -3 }} /></Button>
-            {/*<p className='title-home-container-principal'>Creación de riqueza rediseñada.</p>
-            <p className='text-description-container-principal'>
-              APEX FUNDS GROUP pretende ser una compañía líder en manejo de capital, que contribuye a un panorama de inversión más justo para México y para todo américa latina.
-            </p>
-            <Button className='buttons-home'>Unete al Grupo</Button>
-            <Button className='buttons-home'>Más Información</Button>*/}
           </div>
         </div>
       </div>
+
     );
   }
 }
